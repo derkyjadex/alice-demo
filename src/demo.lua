0591c6af133512e96e7fff22abce557d81b36828
James Deery <james.deery@jadex.org>
2012-10-20 17:57:45 +0100

    Added button to open a FileWidget

--- src/demo.lua
+++ src/demo.lua
@@ -12,6 +12,7 @@ Widget.root:grid_colour(0.2, 0.3, 0.4)
 local toolbar = Toolbar()
 local hello_widget = toolbar:add_button(0.2, 0.5, 0.9)
 local bye_widget = toolbar:add_button(0.0, 0.8, 0.2)
+local open_widget = toolbar:add_button(0.9, 0.6, 0.3)
 toolbar:add_spacer()
 local exit_widget = toolbar:add_button(0.9, 0.3, 0.1)
 
@@ -49,3 +50,28 @@ slider_widget:layout(10, nil, nil, 10, nil, 10)
 Widget.root:add_child(ColourWidget(40, root_height - 210, function(r, g, b)
 	Widget.root:text_colour(r, g, b)
 end))
+
+open_widget:bind_down(function()
+	local shield, file_widget
+
+	shield = Widget()
+	shield:fill_colour(0.2, 0.2, 0.2, 0.5)
+
+	file_widget = FileWidget(function(path)
+		shield:remove()
+		shield:free()
+		file_widget:remove()
+		file_widget:free()
+
+		if path then
+			Widget.root:text(path)
+			Widget.root:invalidate()
+		end
+	end)
+
+	Widget.root:add_child(shield)
+	shield:layout(0, nil, 0, 0, nil, 0)
+
+	Widget.root:add_child(file_widget)
+	file_widget:layout(40, nil, 40, 40, nil, 40)
+end)
